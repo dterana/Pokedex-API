44d0540cc61ad57d4fe6aa3156f3d07f3a185ff7
David TERANA <david.terana@gmail.com>
2016-11-08 14:31:41 -0500

    Basic engine with 3 pokemons

--- Sources/App/Models/Pokemon.swift
+++ Sources/App/Models/Pokemon.swift
@@ -8,60 +8,72 @@ final class Pokemon: Model {
     var index: String
     var name: String
     var type: String
-    var baseDefense: Int
-    var baseAttack: Int
-    var height: Int
-    var weight: Int
+    var baseDefense: String
+    var baseAttack: String
+    var height: String
+    var weight: String
+    var evoLvl: String
+    var evoIndex: String
     
-    
-    init(index: String, name: String, type: String, baseDefense: Int, baseAttack: Int, height: Int, weight: Int) {
+    //Attention - Postgresql field name are only full lowcase
+    init(index: String, name: String, type: String, basedefense: String, baseattack: String, height: String, weight: String, evolvl: String, evoindex: String) {
         self.id = nil
         self.index = index
         self.name = name
         self.type = type
-        self.baseDefense = baseDefense
-        self.baseAttack = baseAttack
+        self.baseDefense = basedefense
+        self.baseAttack = baseattack
         self.height = height
         self.weight = weight
+        self.evoLvl = evolvl
+        self.evoIndex = evoindex
     }
     
     
+    //Attention - Postgresql field name are only full lowcase
     init(node: Node, in context: Context) throws {
         id = try node.extract("id")
         index = try node.extract("index")
         name = try node.extract("name")
         type = try node.extract("type")
-        baseDefense = try node.extract("baseDefense")
-        baseAttack = try node.extract("baseAttack")
+        baseDefense = try node.extract("basedefense")
+        baseAttack = try node.extract("baseattack")
         height = try node.extract("height")
         weight = try node.extract("weight")
+        evoLvl = try node.extract("evolvl")
+        evoIndex = try node.extract("evoindex")
     }
     
     
+    //Attention - Postgresql field name are only full lowcase
     func makeNode(context: Context) throws -> Node {
         return try Node(node: [
             "id": id,
             "index": index,
             "name": name,
             "type": type,
-            "baseDefense": baseDefense,
-            "baseAttack": baseAttack,
+            "basedefense": baseDefense,
+            "baseattack": baseAttack,
             "height": height,
-            "weight": weight
+            "weight": weight,
+            "evolvl": evoLvl,
+            "evoindex": evoIndex
         ])
     }
     
-    
+    //Attention - Postgresql field name are only full lowcase
     static func prepare(_ database: Database) throws {
         try database.create("pokemons") { users in
             users.id()
             users.string("index")
             users.string("name")
             users.string("type")
-            users.int("baseDefense")
-            users.int("baseAttack")
-            users.int("height")
-            users.int("weight")
+            users.string("basedefense")
+            users.string("baseattack")
+            users.string("height")
+            users.string("weight")
+            users.string("evolvl")
+            users.string("evoindex")
         }
     }
     
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -10,10 +10,52 @@ let drop = Droplet(
 
 
 drop.get("id", String.self) { request, pokemonId in
-    return try JSON(node: [
-        "index": "\(pokemonId)",
-        "name": "Pikachu"
-    ])
+    
+    let specificPokemon = try Pokemon.query().filter("index", pokemonId).first()
+
+    
+    
+    if specificPokemon?.evoLvl == "" {
+        
+        return try JSON(node: [
+            "index": "\(specificPokemon!.index)",
+            "name": "\(specificPokemon!.name)",
+            "type": "\(specificPokemon!.type)",
+            "basedefense": "\(specificPokemon!.baseDefense)",
+            "baseattack": "\(specificPokemon!.baseAttack)",
+            "height": "\(specificPokemon!.height)",
+            "weight": "\(specificPokemon!.weight)"
+            ])
+        
+    } else {
+        
+        return try JSON(node: [
+            "index": "\(specificPokemon!.index)",
+            "name": "\(specificPokemon!.name)",
+            "type": "\(specificPokemon!.type)",
+            "basedefense": "\(specificPokemon!.baseDefense)",
+            "baseattack": "\(specificPokemon!.baseAttack)",
+            "height": "\(specificPokemon!.height)",
+            "weight": "\(specificPokemon!.weight)",
+            "evolvl": "\(specificPokemon!.evoLvl)",
+            "evoindex": "\(specificPokemon!.evoIndex)",
+            ])
+    }
 }
+
+
+drop.get("populate") { request in
+    var pokemon = Pokemon(index: "001", name: "Bulbasaur", type: "Poison/Grass", basedefense: "49", baseattack: "49", height: "7", weight: "69", evolvl: "16", evoindex: "002")
+    try pokemon.save()
+    
+    pokemon = Pokemon(index: "002", name: "Ivysaur", type: "Poison/Grass", basedefense: "63", baseattack: "62", height: "10", weight: "130",  evolvl: "32", evoindex: "003")
+    try pokemon.save()
+    
+    pokemon = Pokemon(index: "003", name: "Venusaur", type: "Poison/Grass", basedefense: "83", baseattack: "82", height: "20", weight: "1000",  evolvl: "", evoindex: "")
+    try pokemon.save()
+    
+    return try JSON(node: Pokemon.all().makeNode())
+}
+
 
 drop.run()
