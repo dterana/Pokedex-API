c4f61e7a265c2944e6f7f7ad7567803c8ec921b6
David TERANA <david.terana@gmail.com>
2016-11-15 17:09:00 -0500

    Add some improvement to the filter efficiency

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -10,10 +10,52 @@ drop.preparations += Pokemon.self
 drop.get("api/v0", String.self, String.self) { request, filter, value in
     
     //    Search can be by index, name, ...
-    guard let specificPokemon = try Pokemon.query().filter("\(filter)", value).first()  else {
+    let filters = ["index", "name"]
+    var current_value = value
+    let current_filter = filter.lowercased()
+    
+    if !filters.contains(filter)  {
         throw Abort.badRequest
     }
     
+    
+    switch filter {
+        
+    // Treatment of the value to give it correct format if filter by index
+    case "index":
+        let tmp_value = Int(value)
+        
+        if tmp_value != nil {
+            
+            if tmp_value! < 10 {
+                
+                current_value = "00\(tmp_value!)"
+                
+            } else if tmp_value! < 100 {
+                
+                current_value = "0\(tmp_value!)"
+                
+            }
+        }
+        
+        
+    case "name":
+        
+        current_value = value.capitalized
+        
+    default:
+        current_value = value
+        
+    }
+    
+        
+        
+    guard let specificPokemon = try Pokemon.query().filter("\(filter)", current_value).first()  else {
+        throw Abort.badRequest
+    }
+    
+    
+    //if pokemon can't evolve
     if (specificPokemon.evoLvl) < 0 {
         
         return try JSON(node: [
@@ -27,6 +69,7 @@ drop.get("api/v0", String.self, String.self) { request, filter, value in
             "description": specificPokemon.description
             ])
         
+    //if pokemon can evolve
     } else {
         
         return try JSON(node: [
@@ -42,6 +85,7 @@ drop.get("api/v0", String.self, String.self) { request, filter, value in
             "description": "\(specificPokemon.description)"
             ])
     }
+    
 }
 
 drop.get("api/v0", "all") { request in
